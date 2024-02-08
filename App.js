0eba13ded704d4e69e52857e53afa084d942ade7
ruanJS <rm551098@fiap.com.br>
2024-02-07 23:42:11 -0300

    Update App.js and package.json

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
-export default function App() {
+function App (){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Olá mundo!!!</Text>
+      <Text>Linha 2</Text>
+      <Text>Linha 3</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
+
+export default App;
